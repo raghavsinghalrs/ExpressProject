c14883dd3bc98dcedd3fca275696de6954708e86
Raghav Singhal <84004096+raghavsinghalrs@users.noreply.github.com>
2024-01-29 15:01:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const bodyparser = require('body-parser');
+const app = express();
+const port = 3000;
+
+const adminroute = require('./routes/admin');
+const shoproute = require('./routes/shop');
+
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use(adminroute);
+app.use(shoproute);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+app.listen(port);
